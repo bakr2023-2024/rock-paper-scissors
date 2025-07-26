22aa04611255426b322c0b9c41c4ca1fd9801ef4
bakr2023-2024 <bakr2023.2024@gmail.com>
2025-07-26 05:33:09 +0300

    Display updated score and round result in UI

--- script.js
+++ script.js
@@ -4,22 +4,9 @@ const getHumanChoice = () =>
   prompt("Enter rock, paper or scissors").toLowerCase();
 let humanScore = 0,
   computerScore = 0;
-document
-  .querySelectorAll(".playerAvatar")
-  .forEach((img) =>
-    img.setAttribute(
-      "src",
-      `https://i.pravatar.cc/500?img=${Math.random() * 70 + 1}`
-    )
-  );
-document
-  .querySelectorAll(".choiceBtn")
-  .forEach((btn) =>
-    btn.addEventListener("click", (e) =>
-      console.log(playRound(e.target.dataset.choice, getComputerChoice()))
-    )
-  );
-
+const resultContainer = document.querySelector(".resultContainer");
+const humanScoreDiv = document.querySelector("#humanScore");
+const computerScoreDiv = document.querySelector("#computerScore");
 const playRound = (humanChoice, computerChoice) => {
   if (humanChoice == computerChoice) return 0;
   const scn1 = humanChoice == "rock" && computerChoice == "scissors";
@@ -28,3 +15,40 @@ const playRound = (humanChoice, computerChoice) => {
   if (scn1 || scn2 || scn3) return 1;
   return -1;
 };
+document
+  .querySelectorAll(".playerAvatar")
+  .forEach((img) =>
+    img.setAttribute(
+      "src",
+      `https://i.pravatar.cc/500?img=${Math.random() * 70 + 1}`
+    )
+  );
+document.querySelectorAll(".choiceBtn").forEach((btn) =>
+  btn.addEventListener("click", (e) => {
+    const humanChoice = e.target.dataset.choice;
+    const computerChoice = getComputerChoice();
+    const result = playRound(humanChoice, computerChoice);
+    if (result == 1) {
+      humanScore++;
+      humanScoreDiv.textContent = humanScore;
+      resultContainer.textContent = `You win, ${humanChoice} beats ${computerChoice}`;
+    } else if (result == -1) {
+      computerScore++;
+      computerScoreDiv.textContent = computerScore;
+      resultContainer.textContent = `You lose, ${computerChoice} beats ${humanChoice}`;
+    } else {
+      resultContainer.textContent = `Draw, both players chose ${humanChoice}`;
+    }
+    if (humanScore >= 5) {
+      alert("You won the Game!! Congrats");
+      document
+        .querySelectorAll(".choiceBtn")
+        .forEach((btn) => btn.setAttribute("disabled", "true"));
+    } else if (computerScore >= 5) {
+      alert("You lost the Game!! Hard Luck");
+      document
+        .querySelectorAll(".choiceBtn")
+        .forEach((btn) => btn.setAttribute("disabled", "true"));
+    }
+  })
+);
