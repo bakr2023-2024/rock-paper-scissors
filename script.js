346de97a22f4614776a9ea4c1e842e099f2ed077
bakr2023-2024 <bakr2023.2024@gmail.com>
2025-07-26 03:12:44 +0300

    Add Human choice logic

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const getComputerChoice = (choice = Math.floor(Math.random() * 3)) =>
   choice == 0 ? "rock" : choice == 1 ? "paper" : "scissors";
+const getHumanChoice = () =>
+  prompt("Enter rock, paper or scissors").toLowerCase();
 
