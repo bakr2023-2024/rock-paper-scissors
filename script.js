76d753252412a430cad93fb80a7c6e4f2ffcc9c2
bakr2023-2024 <bakr2023.2024@gmail.com>
2025-07-26 04:40:32 +0300

    Add event listeners to player choices' buttons

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ const getHumanChoice = () =>
   prompt("Enter rock, paper or scissors").toLowerCase();
 let humanScore = 0,
   computerScore = 0;
+document.addEventListener("DOMContentLoaded", () => {
+  document
+    .querySelectorAll(".choiceBtn")
+    .forEach((btn) =>
+      btn.addEventListener("click", (e) =>
+        console.log(playRound(e.target.dataset.choice, getComputerChoice()))
+      )
+    );
+});
 const playRound = (humanChoice, computerChoice) => {
   if (humanChoice == computerChoice) return 0;
   const scn1 = humanChoice == "rock" && computerChoice == "scissors";
@@ -12,29 +21,5 @@ const playRound = (humanChoice, computerChoice) => {
   if (scn1 || scn2 || scn3) return 1;
   return -1;
 };
-const playGame = () => {
-  for (let i = 0; i < 5; i++) {
-    const humanChoice = getHumanChoice();
-    const computerChoice = getComputerChoice();
-    const result = playRound(humanChoice, computerChoice);
-    if (result == 1) {
-      console.log(`You win, ${humanChoice} beats ${computerChoice}`);
-      humanScore++;
-    } else if (result == -1) {
-      console.log(`You lose, ${computerChoice} beats ${humanChoice}`);
-      computerScore++;
-    } else {
-      console.log(`Draw, both chose ${humanChoice}`);
-    }
-  }
-  console.log(
-    `Game Result: ${
-      humanScore == computerScore
-        ? "Draw"
-        : humanScore > computerScore
-        ? "Player wins"
-        : "Computer wins"
-    }`
-  );
-};
+const playGame = () => {};
 playGame();
